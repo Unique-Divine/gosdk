c1db42dc9a3c28dd6efa093946588dfadf14eede
Unique-Divine <realuniquedivine@gmail.com>
2023-07-24 20:48:41 -0500

    default network info

---
+++ netinfo.go
@@ -0,0 +1,19 @@
+package gonibi
+
+type NetworkInfo struct {
+	GrpcEndpoint      string
+	LcdEndpoint       string
+	TmRpcEndpoint     string
+	WebsocketEndpoint string
+	ChainID           string
+}
+
+var (
+	DefaultNetworkInfo = NetworkInfo{
+		GrpcEndpoint:      "localhost:9090",
+		LcdEndpoint:       "http://localhost:1317",
+		TmRpcEndpoint:     "http://localhost:26657",
+		WebsocketEndpoint: "ws://localhost:26657/websocket",
+		ChainID:           "nibiru-localnet-0",
+	}
+)
