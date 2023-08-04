aa096f4f132d5448d5493fa84fd2d8548599d0c4
Unique-Divine <realuniquedivine@gmail.com>
2023-08-04 13:28:43 -0500

    feat(clients.go): augment NibiruClient to include account queries and CometRPC

--- clients.go
+++ clients.go
@@ -1,6 +1,7 @@
 package gonibi
 
 import (
+	"context"
 	"errors"
 
 	xwasm "github.com/CosmWasm/wasmd/x/wasm/types"
@@ -8,41 +9,105 @@ import (
 	xepochs "github.com/NibiruChain/nibiru/x/epochs/types"
 	xoracle "github.com/NibiruChain/nibiru/x/oracle/types"
 	xperp "github.com/NibiruChain/nibiru/x/perp/v2/types"
+	cmtrpcclient "github.com/cometbft/cometbft/rpc/client"
 	"google.golang.org/grpc"
 
+	sdkclient "github.com/cosmos/cosmos-sdk/client"
+	sdkcodec "github.com/cosmos/cosmos-sdk/codec"
+	sdkcodectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	csdk "github.com/cosmos/cosmos-sdk/types"
 	cosmostx "github.com/cosmos/cosmos-sdk/types/tx"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 type NibiruClient struct {
-	ChainId string
-	Keyring keyring.Keyring
-	encCfg  app.EncodingConfig
-	Tx      BroadcastClient
-	Query   QueryClient
+	ChainId          string
+	Keyring          keyring.Keyring
+	EncCfg           app.EncodingConfig
+	Tx               GrpcBroadcastClient
+	Query            QueryClient
+	CometRPC         cmtrpcclient.Client
+	AccountRetriever authtypes.AccountRetriever
+	GrpcClient       *grpc.ClientConn
 }
 
-func NewNibiruClient(chainId string, grpcConn *grpc.ClientConn) (NibiruClient, error) {
-	ensureNibiruPrefix()
+func NewNibiruClient(chainId string, grpcConn *grpc.ClientConn, rpcEndpt string) (NibiruClient, error) {
+	EnsureNibiruPrefix()
 	encCfg := app.MakeEncodingConfig()
 	keyring := keyring.NewInMemory(encCfg.Marshaler)
 	queryClient, err := NewQueryClient(grpcConn)
 	if err != nil {
 		return NibiruClient{}, err
 	}
+	cometRpc, err := NewRPCClient(rpcEndpt, "/websocket")
+	if err != nil {
+		return NibiruClient{}, err
+	}
+	accRetriever := authtypes.AccountRetriever{}
 	return NibiruClient{
 		ChainId: chainId,
 		Keyring: keyring,
-		encCfg:  encCfg,
-		Tx: BroadcastClient{
+		EncCfg:  encCfg,
+		Tx: GrpcBroadcastClient{
 			ServiceClient: cosmostx.NewServiceClient(grpcConn),
 		},
-		Query: queryClient,
+		Query:            queryClient,
+		CometRPC:         cometRpc,
+		AccountRetriever: accRetriever,
+		GrpcClient:       grpcConn,
 	}, err
 }
 
-func ensureNibiruPrefix() {
+// ClientCtx: Docs for args TODO
+//
+//   - tmCfgRootDir: /node0/simd
+//   - Validator.Dir: /node0
+//   - Validator.ClientCtx.KeyringDir: /node0/simcli
+func (nc *NibiruClient) ClientCtx(
+	keyringDir string,
+	tmCfgRootDir string,
+) sdkclient.Context {
+	encCfg := nc.EncCfg
+	return NewClientCtx(ClientCtxBuilder{
+		KeyringDir:        keyringDir, // Not sure what to put here
+		Keyring:           nc.Keyring,
+		TmCfgRootDir:      tmCfgRootDir, // Not sure what to put here
+		ChainID:           nc.ChainId,
+		AccountRetriever:  nc.AccountRetriever,
+		InterfaceRegistry: encCfg.InterfaceRegistry,
+		Codec:             encCfg.Marshaler,
+		TxConfig:          encCfg.TxConfig,
+	})
+}
+
+type ClientCtxBuilder struct {
+	KeyringDir       string
+	Keyring          keyring.Keyring
+	TmCfgRootDir     string
+	ChainID          string
+	AccountRetriever sdkclient.AccountRetriever
+	// app.EncodingConfig.InterfaceRegistry
+	InterfaceRegistry sdkcodectypes.InterfaceRegistry
+	// app.EncodingConfig.Codec
+	Codec sdkcodec.Codec
+	// app.EncodingConfig.TxConfig
+	TxConfig sdkclient.TxConfig
+}
+
+func NewClientCtx(args ClientCtxBuilder) sdkclient.Context {
+	return sdkclient.Context{}.
+		WithKeyringDir(args.KeyringDir).
+		WithKeyring(args.Keyring).
+		WithHomeDir(args.TmCfgRootDir).
+		WithChainID(args.ChainID).
+		WithInterfaceRegistry(args.InterfaceRegistry).
+		WithCodec(args.Codec).
+		WithTxConfig(args.TxConfig).
+		WithAccountRetriever(args.AccountRetriever)
+}
+
+func EnsureNibiruPrefix() {
 	csdkConfig := csdk.GetConfig()
 	nibiruPrefix := app.AccountAddressPrefix
 	if csdkConfig.GetBech32AccountAddrPrefix() != nibiruPrefix {
@@ -75,10 +140,30 @@ func NewQueryClient(
 	}, nil
 }
 
-type BroadcastClient struct {
-	cosmostx.ServiceClient
+type AccountNumbers struct {
+	Number   uint64
+	Sequence uint64
 }
 
-func (txclient *BroadcastClient) BroadcastMsgs() {
-	// TODO
+func (nc *NibiruClient) GetAccountNumbers(address string) (nums AccountNumbers, err error) {
+	queryClient := authtypes.NewQueryClient(nc.GrpcClient)
+	resp, err := queryClient.Account(context.Background(), &authtypes.QueryAccountRequest{
+		Address: address,
+	})
+	if err != nil {
+		return nums, err
+	}
+	// register auth interface
+
+	var acc authtypes.AccountI
+	nc.EncCfg.InterfaceRegistry.UnpackAny(resp.Account, &acc)
+
+	return AccountNumbers{
+		Number:   acc.GetAccountNumber(),
+		Sequence: acc.GetSequence(),
+	}, err
+}
+
+type GrpcBroadcastClient struct {
+	cosmostx.ServiceClient
 }
